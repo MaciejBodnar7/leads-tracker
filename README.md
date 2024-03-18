20bc47038061d9f66e4dd06b43485ec4b9eec30c
MaciejBodnar <maciej.bodnar2@gmail.com>
2024-03-18 13:21:52 +0100

    adding files, styling and basic js

---
+++ README.md
@@ -0,0 +1,1 @@
+# myLeadsTracker | Chrome/Firefox extension
---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('extension by MB.');
+
+let inputBtn = document.getElementById('input-btn');
+
+let myLeads = '';
+let inputEl = document.getElementById('input-el');
+
+inputBtn.addEventListener('click', function () {
+  console.log('text-save-btn2');
+});
